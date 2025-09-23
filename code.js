d915163c15f51b22bd446837ca5b9ac604cb29d8
@ritsondev <ronaldojobs2007@gmail.com>
2025-09-23 01:20:30 +0100

    Upgrade ALL The Project

---
+++ code.js
@@ -0,0 +1,12 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector('i');
+
+menuBtn.addEventListener('click', (e) =>{
+
+    navLinks.classList.toggle("exe");
+
+    const isOpen = navLinks.classList.contains("exe");
+    menuBtnIcon.setAttribute("class", isOpen?"ri-close-line":"ri-menu-line")
+
+})
